8f45576587ebb95c709e480f1550517c7d2ff746
Manomafo <matthieustmart@gmail.com>
2024-07-26 14:28:36 -0300

    feat(API): Cria o repository da entidade User
    - Adiciona o repository para a entidade User, permitindo operações de CRUD.

---
+++ backend/api/src/main/java/com/investpulse/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.investpulse.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.investpulse.api.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
