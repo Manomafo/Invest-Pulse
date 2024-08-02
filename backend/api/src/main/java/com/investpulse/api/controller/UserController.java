72fe38389a146d7eca0f74f8efaf93fc5aeed91d
Manomafo <matthieustmart@gmail.com>
2024-08-02 15:40:24 -0300

    feat: Criação do controller para o UserService
    - Implementação do controller com endpoints para CRUD de Users
    - Endpoints incluem: POST api/user, GET api/user, GET api/user/{email},
      PUT api/user/{email}, DELETE api/user/{email}
    - Adicionado mapeamento de rotas e integração com o service UserService

---
+++ backend/api/src/main/java/com/investpulse/api/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.investpulse.api.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.investpulse.api.dto.UserRequestDTO;
+import com.investpulse.api.dto.UserResponseDTO;
+import com.investpulse.api.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+        private UserService userService;
+
+        public UserController(UserService userService) {
+                this.userService = userService;
+        }
+
+        @GetMapping
+        private ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+                return ResponseEntity.ok(userService.getAllUsers());
+        }
+
+        @GetMapping("/{email}")
+        private ResponseEntity<UserResponseDTO> getUserByEmail(@PathVariable String email) {
+                return ResponseEntity.ok(userService.getUserByEmail(email));
+        }
+
+        @PostMapping
+        private ResponseEntity<UserResponseDTO> postCreateUser(@RequestBody UserRequestDTO user) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(userService.postCreateUser(user));
+        }
+
+        @PutMapping("/{email}")
+        private ResponseEntity<UserResponseDTO> putUpdateUser(@PathVariable String email,
+                        @RequestBody UserRequestDTO user) {
+                return ResponseEntity.ok(userService.putUpdateUser(email, user));
+        }
+
+        @DeleteMapping("/{email}")
+        private ResponseEntity<Object> deleteUserByEmail(@PathVariable String email) {
+                userService.deleteUserByEmail(email);
+                return ResponseEntity.noContent().build();
+        }
+
+}
