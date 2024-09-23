f4090d1a881aa19517b1be52130c6578e8801731
Manomafo <matthieustmart@gmail.com>
2024-09-23 14:28:00 -0300

    feat: Integra back-end com front-end
    - Conectado o back-end da API de usuários ao front-end
      permitindo comunicação entre as duas partes.

--- Invest Pulse/src/cadastro/cadastro.js
+++ Invest Pulse/src/cadastro/cadastro.js
@@ -1,4 +1,55 @@
-document.getElementsByTagName('form').addEventListener('submit', function(event) {
+import axios from "../services/axios";
+
+async function register() {
+    let request = {};
+    
+    request.email = document.getElementById("email").value;
+    request.phoneNumber = document.getElementById("phone-number").value;
+    request.cpf = document.getElementById("cpf").value;
+    request.fullName = document.getElementById("full-name").value;
+    request.password = document.getElementById("password").value;
+
+    
+    if (request.email !== document.getElementById("email-confirm").value) {
+        alert("confirmação do email esta errada");
+
+    }
+
+    if (request.password !== document.getElementById("password-confirm").value) {
+        alert("confirmação da senha esta errada");
+
+    }
+    try {
+
+        const response = await axios.post("/api/user", request);
+        
+        window.location.replace("http://localhost:5173/inicial.html")
+    } catch( error ) { 
+
+    if (error.response.status === 400) {
+      Object.entries(response.data.errors).forEach(([_, value]) => {
+        alert(value)
+        return;
+      })
+    }
+
+    if(error.response.status === 404) {
+      alert("Email ou Senha incorretos")
+      return;
+    }
+
+    if(error.response.status === 409) {
+      alert("Usuario ja existe com este E-mail ou numero de telefone")
+      return;
+    }
+
+    alert("houve um erro")
+
+  }
+}
+
+document.getElementById("register-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    window.location.href = '/public/inicial.html';
+    register();
+    
 });
--- Invest Pulse/src/login/login.js
+++ Invest Pulse/src/login/login.js
@@ -1,3 +1,6 @@
+import axios from '../services/axios'
+
+const eyediv = document.getElementById('eye');
 const eyeElement = document.getElementById("visibility");
 const eyeOffElement = document.getElementById("visibility-off");
 let ispasswordViewEnable = false;
@@ -15,3 +18,42 @@ function togleVisibility() {
     ispasswordViewEnable = true;
   }
 }
+
+async function login() {
+
+  let request = {};
+    
+  request.email = document.getElementById("email").value;
+  request.password = document.getElementById("password").value;
+  
+  try{
+  const response = await axios.post("/api/user/login", request);
+
+
+  
+  window.location.replace("http://localhost:5173/inicial.html")
+  } catch( error ) { 
+
+    if (error.response.status === 400) {
+      Object.entries(response.data.errors).forEach(([_, value]) => {
+        alert(value)
+        return;
+      })
+    }
+
+    if(error.response.status === 404) {
+      alert("Email ou Senha incorretos")
+      return;
+    }
+
+    alert("houve um erro")
+
+  }
+}
+
+eyediv.addEventListener("click", togleVisibility);
+
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  login();
+})
