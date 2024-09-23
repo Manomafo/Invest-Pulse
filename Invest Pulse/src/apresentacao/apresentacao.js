959cd3a06ea5d6a69ff03e9bf518e6722e290b7d
Manomafo <matthieustmart@gmail.com>
2024-09-23 09:50:13 -0300

    feat(UI): Implementa header responsivo
    - Adicionado um header responsivo na página inicial.

--- Invest Pulse/src/apresentacao/apresentacao.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
